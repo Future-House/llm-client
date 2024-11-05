c6adb75f88210e870e6da4f73e4dff7b48167971
chgonz0 <cinder-test@mail.com>
2024-11-05 15:25:58 -0800

    export message client

--- src/llmclient/core.py
+++ src/llmclient/core.py
@@ -1,7 +1,9 @@
+from llmclient.message import LLMMessage
 from llmclient.model import LLMModel
 from llmclient.result import LLMResult
 
 __all__ = [
+    "LLMMessage",
     "LLMModel",
     "LLMResult",
 ]
